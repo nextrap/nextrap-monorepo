b23cdc2c231e20c0b96a2a48375fb51e5309ed15
Enzo Volkmann <enzo@volkmann.dev>
2025-04-11 09:15:24 +0200

    fix test

--- libs/nxa-element-highlighter/src/lib/nxa-element-highlighter.spec.ts
+++ libs/nxa-element-highlighter/src/lib/nxa-element-highlighter.spec.ts
@@ -2,6 +2,6 @@ import { nxaElementHighlighter } from './nxa-element-highlighter';
 
 describe('nxaElementHighlighter', () => {
   it('should work', () => {
-    expect(nxaElementHighlighter()).toEqual('nxa-element-highlighter');
+    expect(nxaElementHighlighter()).toEqual('nxa-element-highlighter@2');
   });
 });
