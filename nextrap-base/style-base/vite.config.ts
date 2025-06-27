33651041bf6b7f42f788e7b62db91c1addac2abd
Matthias <m@tth.es>
2025-06-27 13:42:39 +0200

    fixed style-base example

---
+++ nextrap-base/style-base/vite.config.ts
@@ -0,0 +1,52 @@
+/// <reference types='vitest' />
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import viteServerConfig from '../../utils/vite/config/vite-server-config';
+import viteTestConfig from '../../utils/vite/config/vite-test-config';
+
+export default defineConfig((command) => ({
+  ...viteServerConfig,
+  test: viteTestConfig('nextrap-base/style-base'),
+  publicDir: './public/www',
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/nextrap-elements/nte-burger',
+  plugins: [
+    nxViteTsPaths(),
+    nxCopyAssetsPlugin(['*.md']),
+    dts({
+      entryRoot: 'src',
+      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+    }),
+  ],
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+  // Configuration for building your library.
+  // See: https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    outDir: '../../dist/nextrap-elements/nte-burger',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: 'src/index.ts',
+      name: 'nte-burger',
+      fileName: 'index',
+      // Change this to the formats you want to support.
+      // Don't forget to update your package.json as well.
+      formats: ['es' as const],
+    },
+    rollupOptions: {
+      // External packages that should not be bundled into your library.
+      input: './public/main.ts',
+      external: [],
+    },
+  },
+}));
