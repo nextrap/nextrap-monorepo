68f489a10ec7b243369d011534f588592e923233
Marcel Wasilewski <marcel@wasilewski.io>
2025-07-07 22:13:22 +0200

    implement nte-forms

--- libs/nxa-form/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-## 0.0.0 (2025-05-20)
-
-- Initial release of nxa-form (includes nxa-form-input and nxa-form-group).
--- libs/nxa-form/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/nxa-form-group';
-export * from './lib/nxa-form-input';
-export * from './lib/style-form-group';
-export * from './lib/style-form-input';
