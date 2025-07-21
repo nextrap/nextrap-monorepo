7965b1e1cf7544c0f9ccdeb4b471380bc024c62d
Matthias <m@tth.es>
2025-07-21 11:39:36 +0200

    sync

--- nextrap-elements/nte-nav/src/index.ts
+++ nextrap-elements/nte-nav/src/index.ts
@@ -2,5 +2,7 @@ export * from './lib/nte-nav';
 export * from './lib/nte-navbar';
 export * from './lib/nte-navbar-line';
 
+console.log('Loading nte-nav...');
+
 // The light-dom content of the nav element
 import './nav-global.scss';
