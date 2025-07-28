ae2693a18da33983d1bc0522eecbc40b8efd07f9
Enzo Volkmann <enzo@volkmann.dev>
2025-07-28 10:27:54 +0200

    sample files for elements

--- tools/nextrap-generators/src/generators/options.ts
+++ tools/nextrap-generators/src/generators/options.ts
@@ -3,7 +3,7 @@ import { names } from '@nx/devkit';
 import { Schema } from './schema';
 
 export function resolveOptions(input: Schema) {
-  const name = names(input.name.trim()).fileName;
+  const preparedNames = names(input.name.trim());
   const prefix = (() => {
     switch (input.type) {
       case PackageType.ELEMENTS:
@@ -15,13 +15,17 @@ export function resolveOptions(input: Schema) {
     }
   })();
   const groupDirectoryName = `${nextrap}-${input.type}`;
-  const projectDirName = `${groupDirectoryName}/${prefix}${name}`;
+  const projectDirName = `${groupDirectoryName}/${prefix}${preparedNames.fileName}`;
   return {
     ...input,
     scope: nextrapScope,
-    name,
+    name: preparedNames.fileName,
     prefix,
+    /**
+     * @example nextrap-elements/nte-test-element
+     */
     projectDirName,
+    prefixedClassName: names(`${prefix}${preparedNames.className}`).className,
   };
 }
 
--- tools/nextrap-generators/src/generators/project-generator.ts
+++ tools/nextrap-generators/src/generators/project-generator.ts
@@ -1,11 +1,14 @@
+import { PackageType } from '@nextrap/nextrap-constants';
 import { formatFiles, Tree } from '@nx/devkit';
 import { libraryGenerator } from '@nx/js';
 import { resolveOptions } from './options';
 import { Schema } from './schema';
 import adjustProjectJson from './steps/adjust-project-json';
 import adjustViteConfig from './steps/adjust-vite-config';
+import createElementBaseFiles from './steps/create-element-base-files';
 
 export default async function (tree: Tree, input: Schema) {
+  const generatorSrcRoot = __dirname;
   const options = resolveOptions(input);
 
   // Invoke the default generator with our options
@@ -23,6 +26,10 @@ export default async function (tree: Tree, input: Schema) {
 
   // Adjust server and test config to vite.config.ts
   adjustViteConfig(tree, options);
+
+  if (options.type === PackageType.ELEMENTS) {
+    createElementBaseFiles(tree, options, generatorSrcRoot);
+  }
 
   await formatFiles(tree);
 }
