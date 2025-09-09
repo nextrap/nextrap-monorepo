0b672fca0798f319cdab7bd5b37fa1bdb52605c8
Matthias <m@tth.es>
2025-09-09 13:29:49 +0200

    removed mixin from tree node

--- nextrap-elements/nte-tree-node/src/index.ts
+++ nextrap-elements/nte-tree-node/src/index.ts
@@ -1,7 +1,4 @@
 export * from './components/nte-tree-node/nte-tree-node';
 
-// If mixin exisists, export it
-// export * from './mixins/NteTreeNodeMixin';
-
 /* this bundles light dom styles by default */
 export * from './styles/index.scss';
