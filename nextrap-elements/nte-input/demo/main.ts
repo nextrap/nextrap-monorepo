852302f579209b0e659ac5517a0dc9cff8fd39de
Marcel Wasilewski <marcel@wasilewski.io>
2025-10-14 15:59:44 +0200

    change nte-form structure

---
+++ nextrap-elements/nte-input/demo/main.ts
@@ -0,0 +1,4 @@
+import '@nextrap/style-base';
+import '../src/index.ts';
+import './examples/signature-form-example.ts';
+import './examples/user-registration-form.ts';
--- nextrap-elements/nte-input/src/index.ts
+++ nextrap-elements/nte-input/src/index.ts
@@ -1,4 +1,4 @@
-export * from './components/nte-form-group/nte-form-group';
+export * from './components/nte-input-group/nte-input-group';
 export * from './components/nte-input-signature/nte-input-signature';
 export * from './components/nte-input-tags/nte-input-tags';
 export * from './components/nte-input/nte-input';
