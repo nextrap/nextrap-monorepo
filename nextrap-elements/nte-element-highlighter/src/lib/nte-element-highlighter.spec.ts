707dcf6f3adc0ee8872be59b18171fd220d56f67
Matthias <m@tth.es>
2025-12-05 17:43:21 +0100

    sync

--- nextrap-elements/nte-element-highlighter/src/lib/nte-element-highlighter.spec.ts
+++ nextrap-elements/nte-element-highlighter/src/lib/nte-element-highlighter.spec.ts
@@ -1,24 +1,9 @@
-import { html } from 'lit';
 import { expect } from 'vitest';
-import { render } from 'vitest-browser-lit';
 import { NteElementHighlighter } from './nte-element-highlighter';
 
 describe('nte-element-highlighter', () => {
   it('should create an element', () => {
     const el = new NteElementHighlighter();
     expect(el).toBeInstanceOf(NteElementHighlighter);
-  });
-
-  it('renders', async () => {
-    const screen = render(html`
-      <div class="target">Target</div>
-      <nxa-element-highlighter
-        data-testid="highlight"
-        initiallyShown
-        selector=".target"
-      ></nxa-element-highlighter>
-    `);
-    const element = screen.getByTestId('highlight');
-    await expect.element(element).toBeInTheDocument();
   });
 });
