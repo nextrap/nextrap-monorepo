5b88a61096c5048c52d31f6c088d8ee03735df6e
Plundergames <plundergamesdev@gmail.com>
2026-03-06 13:50:09 +0100

    consent blocker implementation

--- nextrap-layout/ntl-consent-blocker/src/components/ntl-consent-blocker/ntl-consent-blocker.ts
+++ nextrap-layout/ntl-consent-blocker/src/components/ntl-consent-blocker/ntl-consent-blocker.ts
@@ -1,13 +1,10 @@
 import { nextrap_layout, NtlFeatures } from '@nextrap/ntl-core';
-import { create_element, Listen } from '@trunkjs/browser-utils';
-import { html, nothing, unsafeCSS } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
+import { html } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 
 // Styles for the light DOM
-import { resetStyle } from '@nextrap/style-reset';
 
 // Styles for your component's shadow DOM
-import style from './ntl-consent-blocker.scss?inline';
 
 // use nextrap_element for pure elements
 
@@ -18,33 +15,108 @@ const features: NtlFeatures = {
   eventBinding: true, // Switch event binding using @Listen decorators
 };
 
+function stripQuotes(str: string) {
+  return str.replace(/^['"]|['"]$/g, '');
+}
+
 @customElement('ntl-consent-blocker')
 export class NtlConsentBlockerElement extends nextrap_layout(features) {
-  static override styles = [unsafeCSS(style), unsafeCSS(resetStyle)];
+  // static override styles = [unsafeCSS(style), unsafeCSS(resetStyle)];
+  //
+  // // Example of listening to window scroll events
+  // @Listen('click', { target: 'host' })
+  // private onClick(e: Event) {
+  //   if (e.target instanceof HTMLButtonElement && e.target.closest('[data-action="consent"]')) {
+  //     this.#giveConsent();
+  //   }
+  // }
+  //
+  // #giveConsent() {
+  //   const template = this.querySelector(':scope > template') as HTMLTemplateElement;
+  //   if (!template) {
+  //     this.warn(
+  //       'No template found for consented content. Please provide a <template> element as a child of ntl-consent-blocker with the consented content.',
+  //     );
+  //     return;
+  //   }
+  //   Array.from(template.content.children).forEach((el) => {
+  //     const clone = el.cloneNode(true);
+  //     if (clone instanceof HTMLElement) {
+  //       clone.setAttribute('slot', 'consented-content');
+  //       this.appendChild(clone);
+  //     }
+  //   });
+  //   this.consentGiven = true;
+  // }  // static override styles = [unsafeCSS(style), unsafeCSS(resetStyle)];
+  //
+  // // Example of listening to window scroll events
+  // @Listen('click', { target: 'host' })
+  // private onClick(e: Event) {
+  //   if (e.target instanceof HTMLButtonElement && e.target.closest('[data-action="consent"]')) {
+  //     this.#giveConsent();
+  //   }
+  // }
+  //
+  // #giveConsent() {
+  //   const template = this.querySelector(':scope > template') as HTMLTemplateElement;
+  //   if (!template) {
+  //     this.warn(
+  //       'No template found for consented content. Please provide a <template> element as a child of ntl-consent-blocker with the consented content.',
+  //     );
+  //     return;
+  //   }
+  //   Array.from(template.content.children).forEach((el) => {
+  //     const clone = el.cloneNode(true);
+  //     if (clone instanceof HTMLElement) {
+  //       clone.setAttribute('slot', 'consented-content');
+  //       this.appendChild(clone);
+  //     }
+  //   });
+  //   this.consentGiven = true;
+  // }
 
-  // Example of listening to window scroll events
-  @Listen('click', { target: 'host' })
-  private onClick(e: Event) {
-    if (e.target instanceof HTMLButtonElement && e.target.closest('[data-action="save"]')) {
-      this.#giveConsent();
-    }
-  }
-
-  #giveConsent() {
-    const template = this.querySelector(':scope > template');
-    if (!template) {
-      this.warn(
-        'No template found for consented content. Please provide a <template> element as a child of ntl-consent-blocker with the consented content.',
-      );
-      return;
-    }
-    const consentContent = create_element('div', { innerHTML: template.innerHTML, slot: 'consented-content' });
-    this.appendChild(consentContent);
-    this.consentGiven = true;
-  }
-
-  @state()
+  @property({ reflect: true })
   private accessor consentGiven = false;
+
+  // override firstUpdated2(changedProperties: PropertyValues) {
+  //   super.firstUpdated(changedProperties);
+  //   if (this.querySelector(':scope > template') === null) {
+  //     const defaultTemplate = getComputedStyle(this).getPropertyValue('--default-template');
+  //     if (defaultTemplate) {
+  //       this.#copyElementFromString(defaultTemplate, null);
+  //     }
+  //   }
+  //
+  //   if (this.querySelector(':scope > [slot="background"]') === null) {
+  //     const defaultBg = getComputedStyle(this).getPropertyValue('--default-bg');
+  //     if (defaultBg) {
+  //       this.#copyElementFromString(defaultBg, 'background');
+  //     }
+  //   }
+  //
+  //   if (this.querySelector(':scope > template') === null) {
+  //     const defaultContent = getComputedStyle(this).getPropertyValue('--default-content');
+  //     if (defaultContent) {
+  //       this.#copyElementFromString(defaultContent, null);
+  //     }
+  //   }
+  // }
+
+  // #copyElementFromString(htmlString: string, slotName: string | null) {
+  //   const template = document.createElement('template');
+  //   template.innerHTML = stripQuotes(htmlString);
+  //   Array.from(template.content.children).forEach((element) => {
+  //     const clone = element.cloneNode(true);
+  //     if (!(clone instanceof HTMLElement)) {
+  //       return;
+  //     }
+  //
+  //     if (slotName) {
+  //       clone.setAttribute('slot', slotName);
+  //     }
+  //     this.appendChild(clone);
+  //   });
+  // }
 
   override render() {
     return html`
@@ -53,20 +125,15 @@ export class NtlConsentBlockerElement extends nextrap_layout(features) {
           <slot name="background" data-query=":scope > .background | :scope > p:has(img:not(.keep))"></slot>
         </div>
 
-        ${this.consentGiven
-          ? nothing
-          : html`
-              <div id="content" part="content">
-                <slot></slot>
-              </div>
-            `}
-        ${this.consentGiven
-          ? html`
-              <div id="consented-content" part="consented-content">
-                <slot name="consented-content"></slot>
-              </div>
-            `
-          : nothing}
+        <div id="consented-content" part="consented-content">
+          <slot name="consented-content"></slot>
+        </div>
+
+        <div id="pre-consent" part="pre-consent">
+          <slot name="pre-consent"></slot>
+        </div>
+
+        <div id="loading-text">Bitte warten...</div>
       </div>
     `;
   }
