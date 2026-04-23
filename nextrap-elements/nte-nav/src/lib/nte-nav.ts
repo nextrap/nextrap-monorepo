144e0ea13c0302481ed87c437498ccb1470ebe67
Matthias <m@tth.es>
2026-04-23 14:43:03 +0200

    added missing offcanvas

--- nextrap-elements/nte-nav/src/lib/nte-nav.ts
+++ nextrap-elements/nte-nav/src/lib/nte-nav.ts
@@ -1,5 +1,6 @@
 import { isBiggerThanBreakpoint } from '@nextrap/nt-framework';
 import { nextrap_element, NteFeatures } from '@nextrap/nte-core';
+import '@nextrap/nte-offcanvas';
 import { NteOffcanvas } from '@nextrap/nte-offcanvas';
 import { Listen, sleep, waitForLoad } from '@trunkjs/browser-utils';
 import { html, PropertyValues, unsafeCSS } from 'lit';
