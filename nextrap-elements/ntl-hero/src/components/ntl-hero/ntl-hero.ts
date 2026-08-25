c359fb4febcf6c8c6cf82ea61147c52620065260
Matthias Leuffen <matthes@leuffen.de>
2026-08-26 00:06:25 +0200

    refactor(layout): migrate ntl-hero to nextrap_element

--- nextrap-elements/ntl-hero/src/components/ntl-hero/ntl-hero.ts
+++ nextrap-elements/ntl-hero/src/components/ntl-hero/ntl-hero.ts
@@ -1,25 +1,20 @@
-import {Listen, LoggingMixin, sleep, SlotVisibilityMixin, waitForReady} from '@trunkjs/browser-utils';
-import { SubLayoutApplyMixin } from '@trunkjs/content-pane';
-import { html, LitElement, PropertyValues, unsafeCSS } from 'lit';
+import { waitForReady } from '@trunkjs/browser-utils';
+import { html, PropertyValues, unsafeCSS } from 'lit';
 import { customElement } from 'lit/decorators.js';
+import { nextrap_element } from '@nextrap/nt-core';
 
-// Styles for the light DOM
 import { resetStyle } from '@nextrap/style-reset';
-
-// Styles for your component's shadow DOM
 import style from './ntl-hero.scss?inline';
-import {nextrap_layout} from "@nextrap/ntl-core";
-
 
 const features = {
   breakpoints: true,
   subLayoutApply: true,
   slotVisibility: true,
-  eventBinding: true
-}
+  eventBinding: true,
+};
 
 @customElement('ntl-hero')
-export class NtlHero extends nextrap_layout(features) {
+export class NtlHero extends nextrap_element(features) {
   static override styles = [unsafeCSS(style), unsafeCSS(resetStyle)];
 
   protected override async updated(changedProperties: PropertyValues) {
@@ -28,30 +23,15 @@ export class NtlHero extends nextrap_layout(features) {
     this.style.setProperty('--height-offset', `${this.offsetTop}px`);
   }
 
-
   protected override render(): unknown {
     return html`
       <div part="root" id="root">
-        <div id="background" part="background">
-          <slot name="bg"></slot>
-        </div>
-
+        <div id="background" part="background"><slot name="bg"></slot></div>
         <div id="wrapper" part="wrapper">
-          <div id="top-title" part="top-title">
-            <slot name="top-title"></slot>
-          </div>
-
-          <div id="title" part="title">
-            <slot name="title"></slot>
-          </div>
-
-          <div id="content" part="content">
-            <slot></slot>
-          </div>
-
-          <div id="footer" part="footer">
-            <slot name="footer"></slot>
-          </div>
+          <div id="top-title" part="top-title"><slot name="top-title"></slot></div>
+          <div id="title" part="title"><slot name="title"></slot></div>
+          <div id="content" part="content"><slot></slot></div>
+          <div id="footer" part="footer"><slot name="footer"></slot></div>
         </div>
       </div>
     `;
