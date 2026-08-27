d51a55c3aed87b7b40286457cad5e797fc24bef1
Matthias <m@tth.es>
2026-08-27 20:59:21 +0200

    added github pages

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { tjDemoViewerPlugin } from '@trunkjs/vite-demo-viewer';
+import { defineConfig } from 'vite';
+
+const demoInclude = [
+  'nextrap-base/*/demo/**/*.demo.ts',
+  'nextrap-styles/*/demo/**/*.demo.ts',
+  'nextrap-elements/*/demo/**/*.demo.ts',
+  'nextrap-layout/*/demo/**/*.demo.ts',
+];
+
+export default defineConfig(() => ({
+  root: __dirname,
+  publicDir: false,
+  appType: 'custom',
+  server: {
+    port: 4000,
+    host: '0.0.0.0',
+    hmr: true,
+  },
+  optimizeDeps: {
+    entries: demoInclude,
+  },
+  plugins: [
+    tjDemoViewerPlugin({
+      include: demoInclude,
+      route: '/',
+      title: 'Nextrap Demos',
+      build: true,
+    }),
+  ],
+  build: {
+    outDir: './docs',
+    emptyOutDir: false,
+  },
+}));
