15dab4eb5fcc6c259912c086a9118423b1ab637f
Matthias <m@tth.es>
2026-09-10 00:45:46 +0200

    sync

--- nextrap-elements/nte-offcanvas/index.ts
+++ nextrap-elements/nte-offcanvas/index.ts
@@ -1,5 +1,2 @@
 export * from './src/components/nte-offcanvas-pane/nte-offcanvas-pane';
 export * from './src/components/nte-offcanvas/nte-offcanvas';
-
-/* this bundles light dom styles by default */
-import './src/styles/index.scss';
