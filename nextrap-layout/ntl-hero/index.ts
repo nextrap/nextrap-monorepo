34cdd3ba2b4f7ac76aa69cbd59431c7c8a730d76
Matthias Leuffen <matthes@leuffen.de>
2026-09-10 11:58:18 +0200

    fix(ntl-hero): align CSS configuration with layout mixin API

--- nextrap-layout/ntl-hero/index.ts
+++ nextrap-layout/ntl-hero/index.ts
@@ -1,7 +1,2 @@
+// Registriert die Komponente; die Anwendung bindet Light-DOM-Styles über Sass ein.
 export * from './src/components/ntl-hero/ntl-hero';
-
-// If mixin exisists, export it
-// export * from './src/mixins/NtlHeroMixin';
-
-/* this bundles light dom styles by default */
-import './src/styles/index.scss';
